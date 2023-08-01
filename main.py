08235fed37ae82c300672fb1ce72736e6d5887a5
PaulConyngham <paul.steven.conyngham@gmail.com>
2023-08-02 06:59:13 +0800

    massive update

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ st.title("MarketAnythingGPT - Market Anything with GPT ✌️\n")
 st.markdown("---")
 st.header("a demo by [Core Intelligence](%s) for AWAPAC2023" %coreintelligence_url)
 st.markdown("---")
-st.header("Under Construction, please check back later 🏗️👷")
+#st.header("Under Construction, please check back later 🏗️👷")
 
 
 
@@ -40,6 +40,18 @@ openai.api_key = st.secrets["OPENAI_API_KEY"]
 if "openai_model" not in st.session_state:
     st.session_state["openai_model"] = "gpt-3.5-turbo"
 
+
+
+# Functions for each case for the dictionary mapping further down below in the script
+
+def generate_conversation(model, messages):
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages
+    )
+    assistant_msg = response['choices'][0]['message']['content']
+    return assistant_msg
+
 # Utility function to parse the assistant's message into separate categories
 def parse_categories(message):
     # Remove any leading/trailing whitespace and split into lines
@@ -51,61 +63,106 @@ def parse_categories(message):
     # Remove numerical prefixes from each line and return the result
     return [pattern.sub('', line) for line in lines]
 
+def initiate_categorization(product_name, feedback=None):
+    conversation = [
+        {"role": "system", "content": "You are an assistant that helps clarify user product inputs. Your task is to provide a list of possible product categories for a given product, not to assume one."},
+        {"role": "user", "content": f"I have a product named {product_name}. Can you suggest the possible categories for it?"},
+    ]
+    if feedback:
+        conversation.append({"role": "user", "content": feedback})
+
+    assistant_msg = generate_conversation(st.session_state["openai_model"], conversation)
+
+    # Parse the message into categories
+    suggested_categories = parse_categories(assistant_msg)
+
+    # Add 'other' option
+    suggested_categories.append('other')
+
+    # Show categories to the user
+    return suggested_categories
+
+def handle_category_choice(user_choice, suggested_categories):
+    if suggested_categories[user_choice - 1] == 'other':
+        return "Please type your category:"
+    else:
+        return f"You have chosen the category '{suggested_categories[user_choice - 1]}'. Is this correct? (yes/no)"
+
+def confirm_category(user_confirmation):
+    if user_confirmation.lower() in ["yes", "y"]:
+        return True
+    else:
+        return "Please provide feedback to help identify the correct category."
+
+def initiate_product_info_generation(product_name, category, feedback=None):
+    conversation = [
+        {"role": "system", "content": "You are an assistant that provides detailed information about a specific product in a given category. Provide information in a general sense, do not provide specific numbers on items. Finally provide your information in a list format"},
+        {"role": "user", "content": f"I am selling a {product_name} in the {category} category. Can you provide more details about this product?"},
+    ]
+    if feedback:
+        conversation.append({"role": "user", "content": feedback})
+
+    assistant_msg = generate_conversation(st.session_state["openai_model"], conversation)
+    product_details = assistant_msg.strip()
+
+    return f"Detailed Information for {product_name}:\n{product_details}\nIs the above product information correct? (yes/no)"
+
+def confirm_product_info(user_confirmation):
+    if user_confirmation.lower() in ["yes", "y"]:
+        return True
+    else:
+        return "Please specify what is wrong with the product details."
 
 
-def categorize_product(product_name):
-    confirmation = False
-    product_category = ""
-    feedback = ""
+# Define the dictionary mappings
+category_func_dict = {
+    1: initiate_categorization,
+    2: handle_category_choice,
+    3: confirm_category,
+}
+product_info_func_dict = {
+    1: initiate_product_info_generation,
+    2: confirm_product_info,
+}
 
-    while not confirmation:
-        conversation = [
-            {"role": "system", "content": "You are an assistant that helps clarify user product inputs. Your task is to provide a list of possible product categories for a given product, not to assume one."},
-            {"role": "user", "content": f"I have a product named {product_name}. Can you suggest the possible categories for it?"},
-        ]
-        if feedback:
-            conversation.append({"role": "user", "content": feedback})
 
-        # Use the LLM to generate product categories
-        response = openai.ChatCompletion.create(
-            model=st.session_state["openai_model"] ,
-            messages=conversation
-        )
+# Initialize the counter and the current function dictionary
+if 'counter' not in st.session_state:
+    st.session_state.counter = 1
+    st.session_state.current_func_dict = category_func_dict
 
-        # Extract the assistant message from the response
-        assistant_msg = response['choices'][0]['message']['content']
+st.text("What product or service would you like to market?")
 
-        # Parse the message into categories
-        suggested_categories = parse_categories(assistant_msg)
+#"Send a message"
 
-        # Add 'other' option
-        suggested_categories.append('other')
+# React to user input
+if prompt := st.chat_input("Send a message"):
+    # Display user message in chat message container
+    st.chat_message("user").markdown(prompt)
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
 
-        # Show categories to the user
-        for i, category in enumerate(suggested_categories):
-            print(f"{i + 1}. {category}")
+    # Get the function using the dictionary mapping
+    func = st.session_state.current_func_dict.get(st.session_state.counter, lambda x: "Invalid choice")
+    response = func(prompt)
 
-        # Ask user to select a category
-        user_choice = int(input("Please select a category by typing the corresponding number:\n"))
-
-        # Check if user chose 'other'
-        if suggested_categories[user_choice - 1] == 'other':
-            product_category = input("Please type your category:\n")
+    # If the response is True, it means the user has confirmed something
+    if response is True:
+        if st.session_state.current_func_dict is category_func_dict:
+            st.session_state.current_func_dict = product_info_func_dict  # Switch to the product info flow
         else:
-            product_category = suggested_categories[user_choice - 1]
-
-        # Confirm category with user
-        user_confirmation = input(f"You have chosen the category '{product_category}'. Is this correct? (yes/no)\n")
-        if user_confirmation.lower() in ["yes", "y"]:
-            confirmation = True
+            st.session_state.current_func_dict = category_func_dict  # Switch back to the category flow
+        st.session_state.counter = 1  # Reset the counter
+    else:
+        if "no" in prompt.lower():
+            st.session_state.counter -= 1  # Go back to the previous state
         else:
-            print("Please provide feedback to help identify the correct category.")
-            feedback = input()
+            st.session_state.counter += 1  # Go to the next state
 
-    return product_category
-
-
-
-
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        st.markdown(response)
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": response})
 
 
